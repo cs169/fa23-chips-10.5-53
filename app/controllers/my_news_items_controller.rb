0b108016c22fc61f89ca4d177dcec58c694f92c2
Matei Gardea <matei.gardea@berkeley.edu>
2023-12-09 21:22:10 +0000

    [refactor] making it nice

--- app/controllers/my_news_items_controller.rb
+++ app/controllers/my_news_items_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require 'net/http'
 require 'uri'
 require 'json'
@@ -10,10 +11,20 @@ class MyNewsItemsController < SessionController
 
   def new
     @news_item = NewsItem.new
+    @issues = [
+      'Free Speech', 'Immigration', 'Terrorism', 'Social Security and Medicare', 'Abortion',
+      'Student Loans', 'Gun Control', 'Unemployment', 'Climate Change', 'Homelessness', 'Racism', 'Tax Reform',
+      'Net Neutrality', 'Religious Freedom', 'Border Security', 'Minimum Wage', 'Equal Pay'
+    ]
   end
 
   def step1
     @news_item = NewsItem.new
+    @issues = [
+      'Free Speech', 'Immigration', 'Terrorism', 'Social Security and Medicare', 'Abortion',
+      'Student Loans', 'Gun Control', 'Unemployment', 'Climate Change', 'Homelessness', 'Racism', 'Tax Reform',
+      'Net Neutrality', 'Religious Freedom', 'Border Security', 'Minimum Wage', 'Equal Pay'
+    ]
   end
 
   def step2
@@ -25,16 +36,14 @@ class MyNewsItemsController < SessionController
   def save_article
     selected_index = params[:news_item][:selected_article].to_i
     selected_article = params[:news_item][:articles][selected_index.to_s]
-  
     @news_item = NewsItem.new(
-      title: selected_article[:title],
-      description: selected_article[:description],
-      link: selected_article[:link],
+      title:             selected_article[:title],
+      description:       selected_article[:description],
+      link:              selected_article[:link],
       representative_id: params[:news_item][:representative_id],
-      issue: params[:news_item][:issue],
-      rating: params[:news_item][:rating]
+      issue:             params[:news_item][:issue],
+      rating:            params[:news_item][:rating]
     )
-
     if @news_item.save
       redirect_to representative_news_item_path(@representative, @news_item),
                   notice: 'News item was successfully created.'
@@ -66,38 +75,34 @@ class MyNewsItemsController < SessionController
 
   def destroy
     @news_item.destroy
-    redirect_to representative_news_items_path(@representative),
-                notice: 'News was successfully destroyed.'
+    redirect_to representative_news_items_path(@representative), notice: 'News was successfully destroyed.'
   end
 
   private
 
   def fetch_articles(representative, issue)
     uri = URI('https://newsapi.org/v2/everything')
-    params = { 
-      'q' => "#{representative.name} #{issue}",
+    params = {
+      'q'      => "#{representative.name} #{issue}",
       'apiKey' => Rails.application.credentials.NEWS_API_KEY
     }
     uri.query = URI.encode_www_form(params)
     response = Net::HTTP.get_response(uri)
-    if response.is_a?(Net::HTTPSuccess)
-      result = JSON.parse(response.body)
-      
-      # Extract articles from response
-      articles = result['articles']
-  
-      # Extract top 5 articles and their information
-      top_articles = articles.take(5).map do |article|
-        {
-          title: article['title'],
-          url: article['url'],
-          description: article['description']
-        }
-      end
-      # Display or handle the top articles
-      top_articles
-    else
-      puts "Error: #{response.code}"
+
+    return Rails.logger.debug { "Error: #{response.code}" } unless response.is_a?(Net::HTTPSuccess)
+
+    result = JSON.parse(response.body)
+
+    # Extract articles from response
+    articles = result['articles']
+
+    # Extract top 5 articles and their information
+    articles.take(5).map do |article|
+      {
+        title:       article['title'],
+        url:         article['url'],
+        description: article['description']
+      }
     end
   end
 
